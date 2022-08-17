2acc2eccac8c86eb0ab12d14112e99f09e6d917d
Brian Cao <chasoft@users.noreply.github.com>
2022-08-17 20:59:04 +0700

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://docs.chasoft.net',
-  baseUrl: '/docs.chasoft.net/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
