e8db872bda31e4a74cfc84b7e095a1d21e59fe1a
Brian Cao <chasoft@users.noreply.github.com>
2022-08-25 16:02:49 +0700

    wrong github link to edit articles and blog posts

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,14 +38,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/chasoft/docs.chasoft.net/edit/master/docs/',
+            'https://github.com/chasoft/docs.chasoft.net/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/chasoft/docs.chasoft.net/edit/master/blog/',
+            'https://github.com/chasoft/docs.chasoft.net/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
